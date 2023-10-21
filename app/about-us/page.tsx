f7cb1ea46cc9174b87e031f3d81fe5b979bbf26e
mahdiJeddi <mahdiproguni@gmail.com>
2023-10-21 22:22:27 +0330

    MICO project starting for experimental faze | mahdi_jeddi

---
+++ app/about-us/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./about.scss";
+
+function AboutPage() {
+  return (
+    <div className="about__container">
+      <h1> MICO : About us Page </h1>
+      <p>this page : "A"A"C"</p>
+      <p> content static for a while </p>
+      <p>suggestion : ISR & SSG </p>
+    </div>
+  );
+}
+
+export default AboutPage;
